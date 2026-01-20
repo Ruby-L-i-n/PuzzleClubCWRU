70ead876a4bfc83a997b4f67d3cb1e4171bd7e74
Ruby-L-i-n <122896514+Ruby-L-i-n@users.noreply.github.com>
2026-01-20 15:36:36 -0500

    remove about

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
               <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/puzzle-hunt">Puzzle Hunt</Link></li>
-                <li><Link to="/about">About</Link></li>
+                {/* <li><Link to="/about">About</Link></li> */}
                 <li><Link to="/events">Events</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/resources">Resources</Link></li>
